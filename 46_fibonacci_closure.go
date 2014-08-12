bd72737c4d34a51f4c9bf20727045ecb59215573
Ian Molee <ian@tune.com>
2014-08-12 11:46:24 -0700

    Fix opening of sequence: should begin with 1, 1

--- 46_fibonacci_closure.go
+++ 46_fibonacci_closure.go
@@ -5,16 +5,20 @@ import "fmt"
 // fibonacci is a function that returns
 // a function that returns an int.
 func fibonacci() func() int {
-    x, y := 0, 1
-    return func() int {
-        x, y = y, x+y
-        return y
-    }
+	x, y := 0, 0
+	return func() int {
+		if y == 0 {
+			y = 1
+		} else {
+			x, y = y, x+y
+		}
+		return y
+	}
 }
 
 func main() {
-    f := fibonacci()
-    for i := 0; i < 30; i++ {
-        fmt.Println(f())
-    }
+	f := fibonacci()
+	for i := 0; i < 30; i++ {
+		fmt.Println(f())
+	}
 }
